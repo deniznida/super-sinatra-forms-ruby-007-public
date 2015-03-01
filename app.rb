f4a383d592c8e4b34592f39303a9a1f480ce9bdd
deniznida <denizunat@hotmail.com>
2015-03-01 16:11:03 -0500

    done

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :super_hero
+  end
+
+  post '/teams' do
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @heros = params[:team][:members]
+    erb :team
+  end
 end
